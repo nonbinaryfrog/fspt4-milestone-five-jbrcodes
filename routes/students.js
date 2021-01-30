8a85042323d285d33e00f9dbb97ba186fa6ace49
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-01-30 13:17:01 -0500

    "Debugged POST and GET request"

--- routes/students.js
+++ routes/students.js
@@ -12,40 +12,67 @@ router.get("/", function(req, res, next) {
 });
 
 // GET one student
-router.get("/:id", function(req, res, next) {
+router.get("/:id", async function(req, res, next) {
   // get the id first
   let id = req.params.id;
   // if the id exists, get the student with the ID
-  let sql = `SELECT * FROM students WHERE id = ${id}`;
-  db(sql)
-    .then(results => {
-      // send the results in json
+  try {
+    let sql = `SELECT * FROM students WHERE id = ${id}`;
+    let results = await db(sql);
+    if (results.data.length === 1) {
       res.send(results.data);
-    })
-    // catch server error
-    .catch(err => res.status(500).send(err));
-  // if the id doesnt exists, there should be a not found message
+    } else {
+      // if the id doesnt exist, send 404 error message
+      res.status(404).send({ error: "Not Found" });
+    }
+  } catch (err) {
+    // if there is a server message, catch it here
+    res.status(500).send({ error: error.message });
+  }
 });
 
 // INSERT a new student into the DB
 router.post("/", function(req, res, next) {
-  //your code here
   // deconstruct what goes into the body
-  let { firstname, lastname } = req.params.body;
+  let { firstname, lastname } = req.body;
+  // make the post request
   let sql = ` INSERT INTO 
   students ( firstname, lastname ) 
-  VALUES ('${firstname}', '${lastname}');`;
+  VALUES ('${firstname}', '${lastname}')`;
+  // if it goes well, it should return the data
   db(sql).then(results => {
-    db("SELECT * FROM students").then(results => {
-      res.status(201).send(results.data);
-    });
+    db("SELECT * FROM students")
+      .then(results => {
+        res.status(201).send(results.data);
+      })
+      // if it does not go well, give server error
+      .catch(err => res.status(500).send(err));
   });
-  // for some reason this keeps giving me a server error
 });
 
 // DELETE a student from the DB
-router.delete("/:id", function(req, res, next) {
-  //your code here
+router.delete("/:id", async function(req, res, next) {
+  // get the id first
+  let id = req.params.id;
+  // if the id exists, get the student with the ID
+  try {
+    let sql = `SELECT * FROM students WHERE id = ${id}`;
+    let results = await db(sql);
+    if (results.data.length === 1) {
+      sql = `
+    DELETE FROM students
+    WHERE id = ${id}`;
+      await db(sql);
+      results = await db("SELECT * FROM students");
+      res.send(results.data);
+    } else {
+      // if the id doesnt exist, this 404 error will show
+      res.status(404).send({ error: "Not Found" });
+    }
+  } catch (err) {
+    // catch server error here
+    res.status(500).send({ error: error.message });
+  }
 });
 
 module.exports = router;
